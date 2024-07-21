4eae2555ab467005aae686ff8d6803d139c02c12
forbid403 <forbid403@naver.com>
2024-07-21 18:53:54 +0900

    feat(#4): alias 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Main from "./app/Main";
+import Main from "@/app/Main";
 
 const App = () => {
 	return <Routes>
