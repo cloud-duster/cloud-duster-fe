55460b6d6caff110647684a0011a9fe065cd45ea
forbid403 <forbid403@naver.com>
2024-07-30 22:53:13 +0900

    feat(#7): 추억하기/보내주기 라우터 분기

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Main from "@/app/Main";
+import Memory from "./app/memory";
+import Farewell from "./app/farewell";
 
 const App = () => {
 	return <Routes>
 		<Route path="/" element={<Main />} />
+		<Route path="/memory" element={<Memory />} />
+		<Route path="/farewell" element={<Farewell />} />
 	</Routes>;
 };
 
---
+++ src/app/farewell/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Farewell = () => {
+	return <div>보내주기 페이지</div>;
+};
+
+export default Farewell;
