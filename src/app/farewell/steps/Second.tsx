e799d5984bffbb158b141d8afba51bf9c1f75fd6
forbid403 <forbid403@naver.com>
2024-08-12 22:55:15 +0900

    feat(#8): 보내주기 2페이지 구현

--- src/app/farewell/steps/Second.tsx
+++ src/app/farewell/steps/Second.tsx
@@ -1,9 +1,60 @@
-import React from "react";
+import useFarewellStore from "@/state/store";
+import React, { ChangeEvent, useRef } from "react";
+import classNames from "classnames";
+
 
 const Second = () => {
-	return <div>
-        second
-	</div>;
+	const { saveFile, file } = useFarewellStore();
+	const floatingImageRef = useRef<HTMLImageElement>(null);
+
+	const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+		const selectedFile = e.target.files?.[0] || null;
+		
+		if (selectedFile) {
+			saveFile(selectedFile);
+
+			if (floatingImageRef.current){
+				floatingImageRef.current.src = URL.createObjectURL(selectedFile);
+			}
+		}
+	};
+
+	return <>
+		<div className="center">
+			구름을 눌러 <br/>
+			보내줄 사진을 <em className="accent">선택</em> 해 주세요!
+		</div>
+
+		<div>
+			<img 
+				ref={floatingImageRef}
+				className={classNames({
+					floating: file
+				})}
+				id="floating_image"
+			/>
+			<label htmlFor="file">
+				<img
+					src="src/assets/cloud.png"
+					alt="Upload"
+					style={{ 
+						width: 200,
+						height: 100,
+						cursor: "pointer"
+					}}
+				/>
+			</label>
+			<input 
+				accept="image/*"
+				style={{
+					display: "none"
+				}}
+				type="file" 
+				id="file"
+				name="file" 
+				onChange={handleChange} />
+		</div>
+	</>;
 };
 
 export default Second;
--- src/state/store.ts
+++ src/state/store.ts
@@ -2,14 +2,18 @@ import { create } from "zustand";
 
 interface FarewellState {
   currentPage: number;
+  file: null | File;
   nextPage: () => void;
   previousPage: () => void;
+  saveFile: (file: File) => void;
 }
 
 const useFarewellStore = create<FarewellState>()((set) => ({
 	currentPage: 0,
+	file: null,
 	nextPage: () => set((state) => ({ currentPage: state.currentPage + 1 })),
 	previousPage: () => set((state) => ({ currentPage: Math.max(0, state.currentPage - 1) })),
+	saveFile: (file) => set(() => ({ file })),
 }));
 
 export default useFarewellStore;
