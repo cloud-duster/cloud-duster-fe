fa95a5b4328d25e43799a8b21b39ca1f5de5153a
forbid403 <forbid403@naver.com>
2024-09-10 22:31:44 +0900

    feat(#15): 추억하기 로고 구현

--- src/app/components/logo/FarewellLogo.tsx
+++ src/app/components/logo/FarewellLogo.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const FarewellLogo = () => {
-	return <div className="logo-wrapper hover">
+	return <div className="logo-wrapper pointer">
 		<img className="logo-big" src="src/assets/cloud.png" />
 		<img src="src/assets/small-star.png" className="logo-star" />
 		<img src="src/assets/button.png" className="logo-button" />
