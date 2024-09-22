ba51ea9031bc28150e83e72e40d41c77ed22c843
forbid403 <forbid403@naver.com>
2024-09-22 21:46:13 +0900

    feat(#19): detail 레이아웃 구현

--- src/app/memory/MemoryDetail.tsx
+++ src/app/memory/MemoryDetail.tsx
@@ -1,19 +1,24 @@
+import classNames from "classnames";
 import React, { FC } from "react";
 import FarewellLocation from "../farewell/types/FarewellLocation";
-import classNames from "classnames";
 
 interface Props {
-    location?: FarewellLocation;
+	location?: FarewellLocation;
 }
 
 const MemoryDetail: FC<Props> = ({ location = FarewellLocation.MOUNTAIN }) => {
-	return <div className="detail-wrap column">
-		<img
-			className={classNames("detail-image", location.toLowerCase())}
-			src="src/assets/judaeng.png"
-		/>
-		<div>이제는 유행이 지난 짤.. 잘가</div>
-	</div>;
+	return <div className="detail-wrap">
+		<div className="detail-image-wrap">
+			<img
+				className={classNames("detail-image", location.toLowerCase())}
+				src="src/assets/judaeng.png"
+			/>
+			<div className="detail-nickname">익명의 먼지1</div>
+		</div>
+		<p className="detail-content">
+			이제는 유행이 지난 짤.. 잘가이제는 유행이 지난 짤.. 잘가이제는 유행이 지난 짤.. 잘가이제는 유행이 지난 짤.. 잘가이제는 유행이 지난 짤.. 잘가
+		</p>
+	</div >;
 };
 
 export default MemoryDetail;
