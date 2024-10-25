8311702e26dba2254cdb52e2b4870994b3c82f40
forbid403 <forbid403@naver.com>
2024-10-25 21:33:02 +0900

    feat(#16): 스크롤 이슈 수정 및 하단 버튼 추가

--- src/app/summary/index.tsx
+++ src/app/summary/index.tsx
@@ -1,12 +1,23 @@
+import Button from "@/components/Button";
 import "@/css/summary.css";
+import Pages from "@/routes";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import DeletedAmount from "./DeletedAmount";
 import ReducedCarbon from "./ReducedCarbon";
 
 const Summary = () => {
-	return <div className='container column' style={{ overflow: "scroll" }}>
+	const navigate = useNavigate();
+	const handleClickToMain = () => {
+		navigate(Pages.Main);
+	};
+
+	return <div className='summary-container column' style={{ overflow: "scroll" }}>
 		<DeletedAmount />
 		<ReducedCarbon />
+		<Button onClick={handleClickToMain} className="summary-main-button">
+			메인으로
+		</Button>
 	</div>;
 };
 
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,9 +2,9 @@ import classNames from "classnames";
 import React, { FC, PropsWithChildren } from "react";
 
 interface Props {
-    className?: string;
-    onClick?: () => void;
-    disabled?: boolean;
+	className?: string;
+	onClick?: () => void;
+	disabled?: boolean;
 }
 
 const Button: FC<PropsWithChildren<Props>> = ({ children, onClick, className, disabled = false }) => {
@@ -14,8 +14,8 @@ const Button: FC<PropsWithChildren<Props>> = ({ children, onClick, className, di
 		}
 	};
 
-	return <button 
-		onClick={handleClickButton} 
+	return <button
+		onClick={handleClickButton}
 		className={classNames("pixel-button", {
 			disabled
 		}, className)}
