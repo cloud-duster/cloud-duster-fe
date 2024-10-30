2cb42b7003632937f8d9ad1a2005b442a26e5c5c
forbid403 <forbid403@naver.com>
2024-10-30 23:35:12 +0900

    feat(#16): 이미지 및 라벨 추가

--- src/app/summary/component/ReducedList.tsx
+++ src/app/summary/component/ReducedList.tsx
@@ -4,33 +4,33 @@ const ReducedList = () => {
 	return <div className="grid-container">
 		<div className="grid-item-wrapper">
 			<div className="grid-item">
-				<img className="grid-image" src="/assets/button.png" />
+				<img className="grid-image" src="/assets/car.png" />
 			</div>
-			<div className="label">Label 1</div>
+			<div className="label">휘발유<br />자동차 <em className="accent">80km</em></div>
 		</div>
 		<div className="grid-item-wrapper">
 			<div className="grid-item">
-				<img className="grid-image" src="/assets/judaeng.png" />
+				<img className="grid-image" src="/assets/smartphone.png" />
 			</div>
-			<div className="label">Label 2</div>
+			<div className="label">스마트폰 <br /><em className="accent">30</em>번 충전</div>
 		</div>
 		<div className="grid-item-wrapper">
 			<div className="grid-item">
-				<img className="grid-image" src="/assets/clover.png" />
+				<img className="grid-image" src="/assets/computer.png" />
 			</div>
-			<div className="label">Label 3</div>
+			<div className="label">노트북 <br /><em className="accent">30</em>번 충전</div>
 		</div>
 		<div className="grid-item-wrapper">
 			<div className="grid-item">
-				<img className="grid-image" src="/assets/narutomaki.png" />
+				<img className="grid-image" src="/assets/pet.png" />
 			</div>
-			<div className="label">Label 4</div>
+			<div className="label">플라스틱<br />페트병 <em className="accent">20</em>개</div>
 		</div>
 		<div className="grid-item-wrapper">
 			<div className="grid-item">
-				<img className="grid-image" src="/assets/tamagotchi.png" />
+				<img className="grid-image" src="/assets/paper.png" />
 			</div>
-			<div className="label">Label 5</div>
+			<div className="label">A4용지<br /> <em className="accent">20</em>장</div>
 		</div>
 	</div>;
 };
