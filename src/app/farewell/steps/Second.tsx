61f3a79e7807984c83cb01e02f333af0a386aa63
seonghuiB <seonghuibae@navercorp.com>
2024-12-08 18:19:31 +0900

    fix: 확장자명으로 분기 처리

--- src/app/farewell/steps/Second.tsx
+++ src/app/farewell/steps/Second.tsx
@@ -12,7 +12,8 @@ const Second = () => {
 		const selectedFile = e.target.files?.[0] || null;
 
 		if (selectedFile) {
-			const isHeic = selectedFile.type === "image/heic" || selectedFile.type === "image/heif";
+			const isHeic = selectedFile.type === "image/heic" || selectedFile.type === "image/heif" || selectedFile.name.toLowerCase().endsWith(".heic") ||
+				selectedFile.name.toLowerCase().endsWith(".heif");
 			if (isHeic) {
 				alert("캡쳐된 사진으로 올려주세요!");
 				return;
